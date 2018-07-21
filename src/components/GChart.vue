2f37fb374c60d08c7e7efcaacc0214dc14cebadf
Matt Clinton <matt@laralabs.uk>
2018-07-21 17:22:10 +0100

    Add support for multiple groups of charts

--- src/components/GChart.vue
+++ src/components/GChart.vue
@@ -1,5 +1,15 @@
 <template>
-  <div ref="chart"></div>
+  <div class="vue-google-chart">
+    <div v-if="multiple" ref="chartWrap" v-for="(group, groupIndex) in data" :key="'chartGroup' + groupIndex">
+      <div :class="formatClasses(group.classes)" class="chart-group">
+        <div class="chart-group-name" v-if="group.name">{{ group.name }}</div>
+        <div class="chart-wrap" v-for="(chart, index) in group.data" :key="'chart' + groupIndex + index">
+          <div :ref="chart.ref || 'chart' + groupIndex + '-' + index"></div>
+        </div>
+      </div>
+    </div>
+    <div v-else ref="chart"></div>
+  </div>
 </template>
 
 <script>
@@ -36,12 +46,16 @@ export default {
     },
     createChart: {
       type: Function
+    },
+    multiple: {
+      type: Boolean,
+      default: () => (false)
     }
   },
 
   data () {
     return {
-      chartObject: null
+      chartStore: []
     }
   },
 
@@ -49,35 +63,58 @@ export default {
     data: {
       deep: true,
       handler () {
-        this.drawChart()
+        this.redraw()
       }
     },
     options: {
       deep: true,
       handler () {
-        this.drawChart()
+        this.redraw()
       }
     },
     type (value) {
-      this.createChartObject()
-      this.drawChart()
+      this.redraw()
     }
   },
 
   mounted () {
     loadGoogleCharts(this.version, this.settings).then(api => {
       chartsLib = api
-      const chart = this.createChartObject()
-      this.$emit('ready', chart, api)
-      this.drawChart()
+      this.processChartData(api)
     })
   },
 
   methods: {
-    drawChart () {
-      if (!chartsLib || !this.chartObject) return
-      const data = this.getValidChartData()
-      if (data) this.chartObject.draw(data, this.options)
+    processChartData (api) {
+      if (this.multiple && Array.isArray(this.data)) {
+        this.data.forEach((group, groupIndex) => {
+
+          if (Array.isArray(group.data)) group.data.forEach((chart, index) => {
+            const ref = chart.ref || 'chart' + groupIndex + '-' + index;
+            const chartObj = this.createChartObject(ref)
+            this.$emit('ready', chartObj, api)
+            this.drawChart(chartObj, groupIndex, index, ref)
+          })
+        })
+      } else {
+        const ref = 'chart'
+        const chart = this.createChartObject(ref)
+        this.$emit('ready', chart, api)
+        this.drawChart(chart)
+      }
+    },
+
+    drawChart (chart, groupIndex, index, ref) {
+      if (!chartsLib || !this.chartStore.length > 0) return
+      let data = null;
+      if (this.multiple) data = this.getSpecificValidChartData(groupIndex, index, ref)
+      else data = this.getValidChartData()
+      if (data) chart.draw(data, this.options)
+    },
+
+    redraw () {
+      this.chartStore = [];
+      this.processChartData(chartsLib)
     },
 
     getValidChartData () {
@@ -88,22 +125,37 @@ export default {
       return null
     },
 
-    createChartObject () {
+    getSpecificValidChartData (groupIndex, index, ref) {
+      if (this.data[groupIndex].data[index].data instanceof chartsLib.visualization.DataTable) return this.data[groupIndex].data[index].data
+      if (this.data[groupIndex].data[index].data instanceof chartsLib.visualization.DataView) return this.data[groupIndex].data[index].data
+      if (Array.isArray(this.data[groupIndex].data[index].data)) return chartsLib.visualization.arrayToDataTable(this.data[groupIndex].data[index].data)
+      if (this.data[groupIndex].data[index].data === Object(this.data[groupIndex].data[index].data)) return new chartsLib.visualization.DataTable(this.data[groupIndex].data[index].data)
+      return null
+    },
+
+    createChartObject (ref) {
       const createChart = (el, google, type) => {
         if (!type) throw new Error('please, provide chart type property')
         return new google.visualization[type](el)
       }
       const fn = this.createChart || createChart
-      this.chartObject = fn(this.$refs.chart, chartsLib, this.type)
-      this.attachListeners()
-      return this.chartObject
+      let chart = {}
+      if (this.multiple) chart = { [ref]: fn(this.$refs[ref][0], chartsLib, this.type) }
+      else chart = { [ref]: fn(this.$refs[ref], chartsLib, this.type) }
+      const chartIndex = this.chartStore.push(chart)
+      this.attachListeners(chartIndex, ref)
+      return this.chartStore[chartIndex - 1][ref]
     },
 
-    attachListeners () {
+    attachListeners (index, ref) {
       if (!this.events) return
       Object.entries(this.events).forEach(([event, listener]) => {
-        chartsLib.visualization.events.addListener(this.chartObject, event, listener)
+        chartsLib.visualization.events.addListener(this.chartStore[index - 1][ref], event, listener)
       })
+    },
+
+    formatClasses (classes) {
+      if (Array.isArray(classes)) return classes.join(' ')
     }
   }
 }
